a5c86ec9a52ce40f338699d9a43ebcb0a3f819ea
wangjiang <wangjiang@qingsongchou.com>
2020-08-22 15:59:07 +0800

    master

--- src/router/index.js
+++ src/router/index.js
@@ -3,35 +3,7 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-/* Layout */
 import Layout from '@/layout'
-
-/* Router Modules */
-import componentsRouter from './modules/components'
-// import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
-// import nestedRouter from './modules/nested'
-
-/**
- * Note: sub-menu only appear when route children.length >= 1
- * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
- *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
-    icon: 'svg-name'/'el-icon-x' the icon show in the sidebar
-    noCache: true                if set true, the page will no be cached(default is false)
-    affix: true                  if set true, the tag will affix in the tags-view
-    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
- */
 
 /**
  * constantRoutes
@@ -74,13 +46,13 @@ export const constantRoutes = [
   {
     path: '/',
     component: Layout,
-    redirect: '/dashboard',
+    redirect: '/tables',
     children: [
       {
-        path: 'dashboard',
-        component: () => import('@/views/dashboard/index'),
-        name: 'Dashboard',
-        meta: { title: 'Dashboard', icon: 'dashboard', affix: true }
+        path: 'index',
+        component: () => import('@/views/table/complex-table'),
+        name: 'Icons',
+        meta: { title: '活动编辑', icon: 'dashboard', affix: true }
       }
     ]
   }
@@ -92,57 +64,30 @@ export const constantRoutes = [
  */
 export const asyncRoutes = [
   {
-    path: '/icon',
+    path: '/tables',
     component: Layout,
     children: [
       {
         path: 'index',
-        component: () => import('@/views/icons/index'),
+        component: () => import('@/views/table/complex-table'),
         name: 'Icons',
-        meta: { title: 'Icons', icon: 'icon', noCache: true }
+        meta: { title: '活动编辑案例', icon: 'dashboard', noCache: true }
+      }
+    ]
+  },
+  {
+    path: '/Tinymce',
+    component: Layout,
+    children: [
+      {
+        path: 'Tinymce',
+        component: () => import('@/views/components-demo/tinymce'),
+        name: 'Tinymce',
+        meta: { title: '富文本', noCache: true }
       }
     ]
   },
 
-  componentsRouter,
-  tableRouter,
-  {
-    path: '/tab',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/tab/index'),
-        name: 'Tab',
-        meta: { title: 'Tab', icon: 'tab' }
-      }
-    ]
-  },
-
-  {
-    path: '/error',
-    component: Layout,
-    redirect: 'noRedirect',
-    name: 'ErrorPages',
-    meta: {
-      title: 'Error Pages',
-      icon: '404'
-    },
-    children: [
-      {
-        path: '401',
-        component: () => import('@/views/error-page/401'),
-        name: 'Page401',
-        meta: { title: '401', noCache: true }
-      },
-      {
-        path: '404',
-        component: () => import('@/views/error-page/404'),
-        name: 'Page404',
-        meta: { title: '404', noCache: true }
-      }
-    ]
-  },
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
 ]
