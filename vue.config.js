03ab709da2b462eb3e6a37ac628c38fc8da70c48
wangjiang <wangjiang@qingsongchou.com>
2020-09-25 13:52:31 +0800

    1
    Signed-off-by: wangjiang <wangjiang@qingsongchou.com>

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: 'https://admin.huanyubike.com/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
